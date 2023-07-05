cff29eda739c849905954a6ae5c0247282eae3f7
Kaeli Dalton <Kaelidalton@ufl.edu>
2023-07-05 16:44:58 -0400

    beginning work

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,12 @@
 function checkAuth(req, res, next) {
 	// Check req.session.loggedIn to see if the user is logged in
-	// If the user is logged in, call next()
+	if (req.session && req.session.isLoggedIn){
+		// If the user is logged in, call next()
+		return next()
+	}
+	else {
+		res.redirect('/login')
+	}
 	// If the user is not logged in, redirect to /login
 }
 
